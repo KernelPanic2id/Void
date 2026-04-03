6640a2435fb9a788d81739f093bad4286fb52af5
Raphael Taibi <raphael.taibi@epitech.eu>
2026-04-04 00:53:57 +0200

    chore: remove unnecessary blank line for improved code clarity in main.rs

--- packages/signaling-server/src/main.rs
+++ packages/signaling-server/src/main.rs
@@ -64,7 +64,6 @@ struct PeerSession {
     peer_connection: Option<Arc<RTCPeerConnection>>,
 }
 
-
 /// Incoming messages from the Client (Frontend)
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type", rename_all = "kebab-case")]
